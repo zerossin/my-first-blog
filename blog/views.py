bb1ac8fd8cd073a735435734bc7eb6659db54fd1
zerossin <150813710+zerossin@users.noreply.github.com>
2025-10-19 21:29:28 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -16,12 +16,8 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm() 
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -35,7 +31,7 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
